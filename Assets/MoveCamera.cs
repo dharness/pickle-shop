fa2c653de706aa0a013dd93adfa4a4623d4b2755
dharness <dharness.engineer@gmail.com>
2020-06-09 21:15:20 -0400

    .

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float fromZ;
+    public float toZ;
+    public float speed;
+    float direction = 1;
+
+    public void Update()
+    {
+        if (transform.position.z > toZ) { direction = -1; }
+        if (transform.position.z < fromZ) { direction = 1; }
+
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            transform.position.z + (speed * Time.deltaTime * direction)
+        );
+    }
+}
